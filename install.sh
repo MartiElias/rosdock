5ad33fd28581666ade02f69f5610d91652e0dedc
MartiElias <martielias2000@gmail.com>
2025-11-03 19:59:58 +0100

    rosdock connect

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-# chmod +x rosdock
-chmod +x rosdock_connect
+chmod +x rosdock
 sudo cp rosdock /bin/rosdock
-# sudo cp rosdock_connect /bin/rosdock_connect
-# echo "soruce rosdock_connect" >> ~/.bashrc
+
+chmod +x rosdock_connect
+sudo cp rosdock_connect /bin/rosdock_connect
+echo "soruce rosdock_connect" >> ~/.bashrc
