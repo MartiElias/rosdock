8cc91a37ff1cc97284cbdad82c0a26a9393ded31
MartiElias <martielias2000@gmail.com>
2025-11-06 12:39:45 +0100

    GPUs, install and uninstall working.

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
+# Installing rosdock
+echo "Giving executable permissions to rosdock"
 chmod +x rosdock
+echo "Installing rosdock in /bin folder"
 sudo cp rosdock /bin/rosdock
 
-chmod +x rosdock_connect
-sudo cp rosdock_connect /bin/rosdock_connect
-echo "soruce rosdock_connect" >> ~/.bashrc
+# Check exit status of last command
+if [ $? -eq 0 ]; then
+    echo "✅ rosdock installed successfully."
+else
+    echo "❌ Error: command failed."
+fi
+
+# Check if yq is installed
+if ! command -v yq >/dev/null 2>&1; then
+  echo "[rosdock] yq not found, installing..."
+  sudo apt-get update && sudo apt-get install -y yq
+fi
+
+
+# chmod +x rosdock_connect
+# sudo cp rosdock_connect /bin/rosdock_connect
+# echo "soruce rosdock_connect" >> ~/.bashrc
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,13 @@
+# Uninstalling rosdock from /bin folder
+echo "Uninstalling rosdock from /bin"
 sudo rm /bin/rosdock
-sudo rm /bin/rosdock_connect 
-sed -i '/soruce rosdock_connect/d' ~/bashrc
+
+# Check exit status of last command
+if [ $? -eq 0 ]; then
+    echo "✅ Command executed successfully."
+else
+    echo "❌ Error: command failed."
+fi
+
+# sudo rm /bin/rosdock_connect 
+# sed -i '/soruce rosdock_connect/d' ~/bashrc
